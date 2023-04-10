24f3c7f8ccd2a3593c88bd8933a533dd957c30fb
hasan <akonhasan680@gmail.com>
2023-04-10 16:02:44 +0600

    header complete

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,22 +5,22 @@ import { Bars3Icon,XMarkIcon } from '@heroicons/react/24/solid'
 const Header = () => {
     const[open,setOpen]=useState(false)
     return (
-        <div className='flex mt-4 bg-slate-400 p-2 rounded lg:gap-2 justify-between relative '>
+        <div className='flex mt-4 bg-slate-400 p-2 rounded md:gap-2 justify-between relative '>
             <div className='flex items-center gap-4'>
                 <img className='h-16  rounded-full' src="../../public/img/logo.jpg" alt="" />
-                <h1 className='text-2xl font-semibold lg:font-bold'>Job Solutions</h1>
+                <h1 className='text-2xl font-semibold md:font-bold'>Job Solutions</h1>
             </div>
             <nav>
                 <div  className='md:hidden' onClick={()=>setOpen(!open)}>
                     <span>
                         {
-                            open===true?< XMarkIcon className="h-6 w-6 absolute top-6 text-blue-500 left-[80%]" />: < Bars3Icon className="h-6 w-6 absolute text-blue-500 left-[80%] top-6 " />
+                            open===true?< XMarkIcon className="h-6 w-6 absolute top-6 text-blue-500 left-[90%]" />: < Bars3Icon className="h-6 w-6 absolute text-blue-500 left-[90%] top-6 " />
                         }
                     </span>
                
                 
                 </div>
-                <div className={`flex flex-col absolute left-[80%] lg:flex-row lg:left-[35%] lg:gap-8 transition duration-200 ${open?'top-10 lg:top-6':'-top-40 lg:top-6' }`}>
+                <div className={` bg-slate-400 px-2 rounded flex flex-col absolute left-[79%] md:flex-row py-2 md:left-[35%] md:gap-8  duration-200 ${open?'top-16 md:top-4':'-top-48 md:top-6' }`}>
                     <Link className='font-bold' to='/'>Home</Link>
                     <Link className='font-bold' to='/'>Statistics</Link>
                     <Link className='font-bold' to='/'>Applied Jobs</Link>
@@ -29,7 +29,7 @@ const Header = () => {
             </nav>
 
             <div>
-                <button className='absolute mt-4 left-[51%] lg:left-[91%] bg-blue-600 px-4 py-2 rounded text-white font-semibold' >Applied job</button>
+                <button className='absolute mt-3 left-[58%] md:left-[90%] bg-blue-600 px-4 py-2 rounded text-white font-semibold' >Applied job</button>
             </div>
         </div>
     );
